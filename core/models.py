ad03f937686ac75c3f6efbe1ed0f28aa33fb3d48
Fardin_Zand <officialfardinzand@gmail.com>
2024-04-18 19:02:14 +0330

    Add LogicalQuerySet and LogicalManager to core app
    - Created `LogicalQuerySet` and `LogicalManager` classes in `core/managers.py` to provide custom query set methods for logical operations.
    - Added `LogicalManager` as the custom manager for models in `core/models.py` to handle logical queries and operations.
    - Updated `TimeStampMixin` and `LogicalMixin` with the usage of `LogicalManager` for managing logical operations and queryset filtering.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from core.managers import LogicalManager
 
-# Create your models here.
+
+class TimeStampMixin(models.Model):
+    """
+    TimeStamp model mixin:
+
+    fields:
+        - create_at: DateTimeField (auto implement)
+        - modify_at: DateTimeField (auto implement)
+    """
+
+    create_at = models.DateTimeField(auto_now_add=True, editable=False)
+    modify_at = models.DateTimeField(auto_now=True, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class LogicalMixin(models.Model):
+    """
+    Logical model mixin:
+
+    fields:
+        - is_active: BooleanField (default True)
+        - is_deleted: BooleanField (default False)
+    """
+
+    is_active = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
+
+    objects = LogicalManager()
+
+    def delete(self, using=None, keep_parents=False):
+        self.is_deleted = True
+        self.save(update_fields=["is_deleted"])
+
+    def deactivate(self):
+        self.is_active = False
+        self.save(update_fields=["is_active"])
+
+    class Meta:
+        abstract = True
