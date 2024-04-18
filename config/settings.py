0c4198c87106217aa3a08974ef591d4dde84b422
Fardin_Zand <officialfardinzand@gmail.com>
2024-04-18 18:12:28 +0330

    Initialize project structure and customize settings
    🚀 Started Django project by running 'django-admin startproject config .' to create project structure in the current directory.
    
    📁 Created 'config' directory as the project root.
    
    🔧 Customized 'settings.py' to configure project settings:
    - Defined base directory (`BASE_DIR`) using `Pathlib`.
    - Set `SECRET_KEY`, `DEBUG`, `ALLOWED_HOSTS`, `INSTALLED_APPS`, `MIDDLEWARE`, `ROOT_URLCONF`, `TEMPLATES`, `WSGI_APPLICATION`, `DATABASES`, `AUTH_PASSWORD_VALIDATORS`, `LANGUAGE_CODE`, `TIME_ZONE`, `USE_I18N`, `USE_TZ`, `STATIC_URL`, `MEDIA_URL`, `MEDIA_ROOT`, `DEFAULT_AUTO_FIELD`, and `STATICFILES_DIRS` for development.
    
    🌐 Configured 'urls.py' to define URL patterns:
    - Set a default route to a 'TemplateView' displaying 'firstpage.html'.
    - Enabled admin interface URL and static/media file serving during development.
    
    These changes establish the project structure, configure essential settings, and define initial URL routing for the Django application.

---
+++ config/settings.py
@@ -0,0 +1,97 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'django-insecure-fkjp)700n4ow2yyec72svp81thskj%q3uflo_$7t0##6f08(d='
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'config.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / "templates"],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# Password validation
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'Asia/Tehran'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Serving
+STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
+# Default primary key field type
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Mode Handling:
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / "storage/static"]
---
+++ config/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='firstpage.html'), name='firstpage'),
+]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+
+    urlpatterns += (
+        [path("admin/", admin.site.urls)]
+        + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+        + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    )
