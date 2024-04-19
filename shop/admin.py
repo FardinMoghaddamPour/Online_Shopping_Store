1f55d554381e3a6b2742b94d05b7632aa140f402
Fardin_Zand <officialfardinzand@gmail.com>
2024-04-19 11:54:21 +0330

    🔧📝 Organized admin registrations for models across apps
    Refactored admin registrations to enhance readability and maintainability by grouping model registrations logically within respective admin files.
    
    Changes:
    - Consolidated admin registrations for CustomUser and Address models in account/admin.py.
    - Organized admin registrations for Category, Product, Inventory, WishlistProduct, Wishlist, Order, OrderItem, Discount, and Coupon models in shop/admin.py.
    
    This commit improves code organization and makes it easier to manage admin configurations across different apps.

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import (
+    Category,
+    Product,
+    Inventory,
+    WishlistProduct,
+    Wishlist,
+    Order,
+    OrderItem,
+    Discount,
+    Coupon
+)
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Inventory)
+admin.site.register(WishlistProduct)
+admin.site.register(Wishlist)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Discount)
+admin.site.register(Coupon)
