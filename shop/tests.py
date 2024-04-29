8d3ea80eb540e394fbbcaf01d803e0286811aee5
Fardin_Zand <officialfardinzand@gmail.com>
2024-04-29 10:51:11 +0330

    Added unit tests for Category model
    Added unit tests to ensure the functionality of the Category model's CRUD operations. Four tests were implemented:
    1. `test_create_category`: Tests the creation of a new category.
    2. `test_read_category`: Tests retrieving a category from the database.
    3. `test_update_category`: Tests updating an existing category.
    4. `test_delete_category`: Tests deleting a category from the database.
    
    These tests cover the basic CRUD operations for the Category model, providing confidence in its behavior. All tests passed successfully.

--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from .models import Category
 
-# Create your tests here.
+
+class CategoryModelTest(TestCase):
+
+    def test_create_category(self):
+
+        category = Category.objects.create(name='Test Category')
+
+        self.assertEqual(category.name, 'Test Category')
+
+    def test_read_category(self):
+
+        category = Category.objects.create(name='Test Category')
+
+        retrieved_category = Category.objects.get(name='Test Category')
+
+        self.assertEqual(category, retrieved_category)
+
+    def test_update_category(self):
+
+        category = Category.objects.create(name='Test Category')
+
+        category.name = 'Updated Test Category'
+        category.save()
+
+        updated_category = Category.objects.get(name='Updated Test Category')
+
+        self.assertEqual(updated_category.name, 'Updated Test Category')
+
+    def test_delete_category(self):
+
+        category = Category.objects.create(name='Test Category')
+
+        category.delete()
+
+        # noinspection PyTypeChecker
+        with self.assertRaises(Category.DoesNotExist):
+            Category.objects.get(name='Test Category')
