1e205e9ba368812dfe4a311e4493e7e139a15b74
Fardin_Zand <officialfardinzand@gmail.com>
2024-04-29 10:59:33 +0330

    Implemented unit tests for Inventory model
    Added unit tests to ensure the functionality of the Inventory model's CRUD operations. Four tests were created:
    1. `test_create_inventory`: Tests the creation of a new inventory.
    2. `test_read_inventory`: Tests retrieving an inventory from the database.
    3. `test_update_inventory`: Tests updating an existing inventory.
    4. `test_delete_inventory`: Tests deleting an inventory from the database.
    
    These tests verify that the Inventory model behaves as expected and that the CRUD operations are functioning correctly. All tests passed successfully.

--- shop/tests.py
+++ shop/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
-from .models import Category
+from .models import (
+    Category,
+    Inventory
+)
 
 
 class CategoryModelTest(TestCase):
@@ -38,3 +41,44 @@ class CategoryModelTest(TestCase):
         # noinspection PyTypeChecker
         with self.assertRaises(Category.DoesNotExist):
             Category.objects.get(name='Test Category')
+
+
+class InventoryModelTest(TestCase):
+
+    def test_create_inventory(self):
+
+        inventory = Inventory.objects.create(name='Test Inventory', capacity=100)
+
+        self.assertEqual(inventory.name, 'Test Inventory')
+        self.assertEqual(inventory.capacity, 100)
+
+    def test_read_inventory(self):
+
+        inventory = Inventory.objects.create(name='Test Inventory', capacity=100)
+
+        retrieved_inventory = Inventory.objects.get(name='Test Inventory')
+
+        self.assertEqual(inventory, retrieved_inventory)
+
+    def test_update_inventory(self):
+
+        inventory = Inventory.objects.create(name='Test Inventory', capacity=100)
+
+        inventory.name = 'Updated Test Inventory'
+        inventory.capacity = 200
+        inventory.save()
+
+        updated_inventory = Inventory.objects.get(name='Updated Test Inventory')
+
+        self.assertEqual(updated_inventory.name, 'Updated Test Inventory')
+        self.assertEqual(updated_inventory.capacity, 200)
+
+    def test_delete_inventory(self):
+
+        inventory = Inventory.objects.create(name='Test Inventory', capacity=100)
+
+        inventory.delete()
+
+        # noinspection PyTypeChecker
+        with self.assertRaises(Inventory.DoesNotExist):
+            Inventory.objects.get(name='Test Inventory')
